bc414d360ff48629fe363f0888a1b678f78ed75d
Varun Naharia <varun.naharia@gmail.com>
2017-04-21 05:45:32 +0530

    initial commit

--- TableInputView/ViewController.swift
+++ TableInputView/ViewController.swift
@@ -8,10 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tblView: UITableView!
+    @IBOutlet weak var txtInputField: UITextField!
+    let data:[String] = ["1","2","3","4","5","6","7"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        txtInputField.inputView = tblView
+        let toolBar = UIToolbar().ToolbarPiker(selector: #selector(ViewController.dismissPicker))
+        
+        txtInputField.inputAccessoryView = toolBar
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +26,51 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = data[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        txtInputField.text = data[indexPath.row]
+    }
+    
+    func dismissPicker(){
+        if(txtInputField.text == "")
+        {
+            txtInputField.text = data[0]
+        }
+        view.endEditing(true)
+    }
 
 
+}
+
+extension UIToolbar {
+    
+    func ToolbarPiker(selector : Selector) -> UIToolbar {
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.plain, target: self, action: selector)
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([ spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        return toolBar
+    }
+    
 }
 
